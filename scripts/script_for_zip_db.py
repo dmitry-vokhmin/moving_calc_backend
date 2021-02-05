6cdcfb41e27177db2204ee80f99696af5eeb73c9
Dmitry <boxdima1@gmail.com>
2021-02-05 10:43:29 -0500

    last version

--- scripts/script_for_zip_db.py
+++ scripts/script_for_zip_db.py
@@ -3,8 +3,6 @@ import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
-
-
 session = requests.Session()
 retry = Retry(connect=3, backoff_factor=0.5)
 adapter = HTTPAdapter(max_retries=retry)
@@ -18,7 +16,7 @@ with open("uszips.csv", encoding='utf-8') as r_file:
             count += 1
             continue
         session.post('http://127.0.0.1:8080/zip_code/', json={"zip_code": f"{row[0]}",
-                                                               "city": f"{row[3]}",
-                                                               "state": f"{row[4]}"})
+                                                              "city": f"{row[3]}",
+                                                              "state": f"{row[4]}"})
 
 print("END")
